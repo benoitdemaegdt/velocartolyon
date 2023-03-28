e00796a0c797660993a15b9541ffdbb9dd34e5f9
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-03-28 12:15:35 +0200

    fix header

--- client/components/app/Header.vue
+++ client/components/app/Header.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="relative z-50 bg-white shadow">
     <div class="max-w-7xl mx-auto px-4 sm:px-6">
-      <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+      <div class="flex justify-start items-center py-6 md:justify-start md:space-x-10">
         <div class="flex justify-start lg:w-0 lg:flex-1">
-          <NuxtLink to="/">
-            <span class="sr-only">Vélo Carto Lyon</span>
-          </NuxtLink>
-          <span class="ml-2 text-2xl sm:hidden font-extrabold text-official">Vélo Carto Lyon</span>
+          <span class="ml-2 text-2xl font-extrabold text-blue-600">Vélo Carto Lyon</span>
         </div>
       </div>
     </div>
