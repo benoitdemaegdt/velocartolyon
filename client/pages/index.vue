8b1f568d7c8cbeb8885e272e78993758efff0c02
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-03-28 11:56:15 +0200

    add map

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="h-full w-full">
-    <h1>Hello World</h1>
-<!--    <div id="map" class="h-full" />-->
+    <div id="map" class="h-full" />
   </div>
 </template>
 
 <script setup>
+import style from '@/assets/style.json'
+import maplibregl from 'maplibre-gl'
+import 'maplibre-gl/dist/maplibre-gl.css'
+
 // https://github.com/nuxt/framework/issues/3587
 definePageMeta({
   pageTransition: false,
   layout: 'fullscreen'
+})
+
+onMounted(() => {
+  const map = new maplibregl.Map({
+    container: 'map',
+    style,
+    center: [4.8312188, 45.757198],
+    zoom: 13
+  })
+  map.addControl(new maplibregl.NavigationControl({ showCompass: false }), 'top-left')
+  map.addControl(new maplibregl.FullscreenControl(), 'top-right')
 })
 </script>
