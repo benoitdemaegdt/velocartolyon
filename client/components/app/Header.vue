3e0e4edab4fa56ac78006b80b38d6bea19286b08
Benoit Demaegdt <benoit.demaegdt@gmail.com>
2023-03-28 11:50:45 +0200

    setup tailwind

---
+++ client/components/app/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="relative z-50 bg-white shadow">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6">
+      <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+        <div class="flex justify-start lg:w-0 lg:flex-1">
+          <NuxtLink to="/">
+            <span class="sr-only">Vélo Carto Lyon</span>
+          </NuxtLink>
+          <span class="ml-2 text-2xl sm:hidden font-extrabold text-official">Vélo Carto Lyon</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ client/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-full w-full">
+    <h1>Hello World</h1>
+<!--    <div id="map" class="h-full" />-->
+  </div>
+</template>
+
+<script setup>
+// https://github.com/nuxt/framework/issues/3587
+definePageMeta({
+  pageTransition: false,
+  layout: 'fullscreen'
+})
+</script>
