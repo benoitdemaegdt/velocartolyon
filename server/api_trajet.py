db1ee190afd181798a169456eb0472cfd4ec751f
Mathieu NICOLLE <mnicolle.utc@gmail.com>
2023-03-28 13:57:50 +0200

    feat: API response based on two adress

--- server/api_trajet.py
+++ server/api_trajet.py
@@ -8,15 +8,25 @@ Created on Mon Sep 12 19:19:23 2022
 import os
 import pandas as pd
 import requests
+import json
+from geopy.geocoders import Nominatim
 from credentials import API_KEY
 
+def get_gps_coordinates(address):
+    geolocator = Nominatim(user_agent="myGeocoder")
+    location = geolocator.geocode(address)
+    if location is not None:
+        return location.latitude, location.longitude
+    else:
+        return None
+    
+def define_str_point(lat_value,long_value):
+    point = f"{lat_value},{long_value}"
+    return point
 
 def call_api(start_point, end_point):
-    
     key=API_KEY
-    start_coordinates = "37.421999,-122.084057"  # Replace with your starting point coordinates
-    end_coordinates = "37.7749,-122.4194"  # Replace with your ending point coordinates
-    url = f"https://maps.googleapis.com/maps/api/directions/json?origin={start_coordinates}&destination={end_coordinates}&key={key}"
+    url = f"https://maps.googleapis.com/maps/api/directions/json?origin={start_point}&destination={end_point}&key={key}"
     response = requests.get(url)
     data = response.json()
     print(data)
@@ -32,12 +42,24 @@ def call_api(start_point, end_point):
     else:
         print(f"Error: {data['status']}")
     """
-        
+    with open('output.txt', 'w') as file:
+        # Write the JSON content to the file
+        json.dump(data, file)
+
+def get_traject(adress_start,adress_end):
+    start_lat,start_long = get_gps_coordinates(address_start)
+    end_lat,end_long = get_gps_coordinates(address_end)
+    start_point = define_str_point(start_lat,start_long)
+    end_point = define_str_point(end_lat,end_long)
+    call_api(start_point,end_point)
+    
 
 
 if __name__ == '__main__':
+    address_start = "35 Cours Emile Zola 69100 Villeurbanne"
+    address_end = "10 rue de Montbrillant 69003 Lyon"
     # Main function
-    call_api(1,2)
+    get_traject(address_start,address_end)
     
     
 
